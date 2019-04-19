eb12be4e3b658c227daa3cd9aeb7bd38f3f216a8
SHASHANK BHAT <shashankbhat1800@gmail.com>
2019-04-19 12:47:00 +0530

    1.3

--- app/src/main/java/com/shashankbhat/graph/DrawGraph.java
+++ app/src/main/java/com/shashankbhat/graph/DrawGraph.java
@@ -12,55 +12,66 @@ import java.util.ArrayList;
 
 public class DrawGraph {
     private ArrayList<Point> points;
-    private Paint paint;
+    private Paint coordinate,line;
     private Bitmap bg;
     private Context context;
+    private Canvas canvas;
 
     public DrawGraph(Context context) {
         this.context = context;
+
+        bg = Bitmap.createBitmap(2000,2000,Bitmap.Config.ARGB_8888);
+
+        canvas = new Canvas(bg);
+        canvas.translate(0,canvas.getHeight());
+        canvas.scale(10,-10);
+
+        points = new ArrayList<>();
+        coordinate = new Paint();
+        line = new Paint();
+
+        coordinate.setColor(Color.BLUE);
+        line.setColor(Color.GREEN);
+
+    }
+
+    public void addPoint(float x,float y){
+        points.add(new Point(x,y));
+    }
+
+    public void setCoordinateColor(int color){
+        coordinate.setColor(color);
+    }
+    public void setLineColor(int color){
+        line.setColor(color);
     }
 
     @SuppressLint("NewApi")
     public BitmapDrawable drawGraph(){
-        points = new ArrayList<>();
-        paint = new Paint();
-        bg = Bitmap.createBitmap(400,400,Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bg);
-        canvas.translate(0,canvas.getHeight());
-        canvas.scale(5,-5);
 
-        points.add(new Point(10,10));
-        points.add(new Point(40,60));
-        points.add(new Point(60,100));
-
-        paint.setStrokeWidth(2);
-        paint.setColor(Color.RED);
         DrawCoordinate(canvas,canvas.getHeight(),canvas.getWidth());
-
-        paint.setStrokeWidth(1);
-        paint.setColor(Color.BLUE);
         DrawContinuousLines(canvas);
 
         return new BitmapDrawable(bg);
     }
 
     private void DrawContinuousLines(Canvas canvas) {
-
-        paint.setColor(Color.BLUE);
+        line.setStrokeWidth(1);
 
         Point present = points.get(1);
         Point previous = points.get(0);
 
         for (Point point : points) {
-            canvas.drawLine(previous.getX(), previous.getY(), present.getX(), present.getY(), paint);
+            canvas.drawLine(previous.getX(), previous.getY(), present.getX(), present.getY(), line);
             previous = present;
             present = point;
         }
     }
 
     private void DrawCoordinate(Canvas canvas,int height, int width) {
-        paint.setColor(Color.GRAY);
-        canvas.drawLine(0, 0, 0, height, paint);
-        canvas.drawLine(0, 0,  width,0, paint);
+        coordinate.setStrokeWidth(2);
+        canvas.drawLine(0, 0, 0, height, coordinate);
+        canvas.drawLine(0, 0,  width,0, coordinate);
     }
 }
+
