f08aaf8232640ebcbab399e174fdeb5014d5ade7
SHASHANK BHAT <shashankbhat1800@gmail.com>
2019-04-21 18:24:01 +0530

    Fixes-Unit line and Background Color

--- app/src/main/java/com/shashankbhat/graph/DrawGraph.java
+++ app/src/main/java/com/shashankbhat/graph/DrawGraph.java
@@ -1,77 +1,107 @@
 package com.shashankbhat.graph;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
+import android.graphics.*;
 import android.graphics.drawable.BitmapDrawable;
 
 import java.util.ArrayList;
 
 public class DrawGraph {
     private ArrayList<Point> points;
-    private Paint coordinate,line;
+    private Paint background,line,unitLine;
     private Bitmap bg;
-    private Context context;
     private Canvas canvas;
+    private float max_x=0,max_y=0;
 
-    public DrawGraph(Context context) {
-        this.context = context;
-
-        bg = Bitmap.createBitmap(2000,2000,Bitmap.Config.ARGB_8888);
-
-        canvas = new Canvas(bg);
-        canvas.translate(0,canvas.getHeight());
-        canvas.scale(10,-10);
+    public DrawGraph() {
 
         points = new ArrayList<>();
-        coordinate = new Paint();
-        line = new Paint();
 
-        coordinate.setColor(Color.BLUE);
-        line.setColor(Color.GREEN);
+        background = new Paint();
+        line = new Paint();
+        unitLine = new Paint();
+
+        background.setColor(Color.parseColor("#F0FAF7"));
+
+        unitLine.setColor(Color.parseColor("#6DAE9A"));
+        line.setColor(Color.parseColor("#718DAD"));
+        unitLine.setStrokeWidth(20f);
+        line.setStrokeWidth(30f);
 
     }
 
     public void addPoint(float x,float y){
-        points.add(new Point(x,y));
+        if(max_x > x && max_y > y)
+            points.add(new Point(x,y));
+
+        else {
+            if(max_x < x)
+                max_x = x;
+            if(max_y < y)
+                max_y = y;
+            points.add(new Point(x,y));
+        }
     }
 
-    public void setCoordinateColor(int color){
-        coordinate.setColor(color);
+    public void setBackgroundColor(String color){
+        background.setColor(Color.parseColor(color));
     }
-    public void setLineColor(int color){
-        line.setColor(color);
+    public void setLineColor(String color){
+        line.setColor(Color.parseColor(color));
+    }
+
+    public void setUnitLineWidht(Float widht){
+        background.setStrokeWidth(widht);
+    }
+    public void setLineWidth(Float widht){
+        line.setStrokeWidth(widht);
     }
 
     @SuppressLint("NewApi")
     public BitmapDrawable drawGraph(){
 
-        DrawCoordinate(canvas,canvas.getHeight(),canvas.getWidth());
+        recalculateXY();
+
+        bg = Bitmap.createBitmap(5000,5000,Bitmap.Config.ARGB_8888);
+
+        canvas = new Canvas(bg);
+        canvas.translate(0,canvas.getHeight());
+        canvas.scale(1,-1);
+
+        //draw background
+        canvas.drawRect(0, 0, canvas.getHeight(), canvas.getWidth(), background);
+
+        UnitGraph(canvas);
         DrawContinuousLines(canvas);
 
         return new BitmapDrawable(bg);
     }
 
-    private void DrawContinuousLines(Canvas canvas) {
-        line.setStrokeWidth(1);
-
-        Point present = points.get(1);
-        Point previous = points.get(0);
-
-        for (Point point : points) {
-            canvas.drawLine(previous.getX(), previous.getY(), present.getX(), present.getY(), line);
-            previous = present;
-            present = point;
+    private void recalculateXY() {
+        for(int i=0;i<points.size();i++){
+            points.get(i).x = (5000/max_x)*points.get(i).x;
+            points.get(i).y = (5000/max_y)*points.get(i).y;
         }
     }
 
-    private void DrawCoordinate(Canvas canvas,int height, int width) {
-        coordinate.setStrokeWidth(2);
-        canvas.drawLine(0, 0, 0, height, coordinate);
-        canvas.drawLine(0, 0,  width,0, coordinate);
+    private void UnitGraph(Canvas canvas) {
+        float max = 5000;
+        for(float i=1000;i<max;i+= 1000){
+            canvas.drawLine(0,i,canvas.getHeight(),i,unitLine);
+            canvas.drawLine(i,0,i,canvas.getHeight(),unitLine);
+        }
+    }
+
+    private void DrawContinuousLines(Canvas canvas) {
+
+        Point present = points.get(0);
+        Point previous;
+
+        for (int i=1;i<points.size();i++) {
+            previous = present;
+            present = points.get(i);
+            canvas.drawLine(previous.getX(), previous.getY(), present.getX(), present.getY(), line);
+        }
     }
 }
 
